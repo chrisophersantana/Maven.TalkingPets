6c6809e580d0bb6dafbe13cf0d4655f7893306fb
Christopher Santana <chris@zipcoders-MacBook-Pro-6.local>
2019-11-02 12:01:13 -0400

    trying first save

---
+++ src/main/java/io/zipcoder/polymorphism/Pet.java
@@ -0,0 +1,17 @@
+package io.zipcoder.polymorphism;
+
+
+public class Pet {
+    private String speak;
+    private String name;
+
+
+ Pet pet = new pet(String speak,String name) {
+
+
+     this.speak = speak;
+     this.name = name;
+
+ }
+
+ }
